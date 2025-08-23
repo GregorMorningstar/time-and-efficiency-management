a718f54deaaa5cb6d7a17be04c53c46ca3299e7c
GregorMorningstar <gregormorningstar@gmail.com>
2025-08-23 12:26:45 +0200

    implement chat mode

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
+    Route::post('/chat', [ChatController::class, 'store'])->name('chat.store');
+}); 
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
+
